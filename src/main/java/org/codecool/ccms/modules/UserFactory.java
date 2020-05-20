9bed379c80af32f2b060161a66c4a66a5989ecfd
Alek <sforaaleksander@gmail.com>
2020-05-20 16:16:09 +0200

    refactor switch cases to use Role enums

--- src/main/java/org/codecool/ccms/modules/UserFactory.java
+++ src/main/java/org/codecool/ccms/modules/UserFactory.java
@@ -9,32 +9,33 @@ public class UserFactory {
         this.userDao = userDao;
     }
 
-    public User makeUser(int id, String name, String surname, String email, String password, int roleId) {
+    public User makeUser(int id, String name, String surname, String email,
+                         String password, Role role, Attendance attendance) {
 
         //TODO make enums with roles
-        switch (roleId){
-            case 1:
+        switch (role.toString()){
+            case "STUDENT":
                 Module module = Module.ADVANCED;
                 //TODO get student module
-                User student = new Student(id, name, surname, email, password, roleId, module);
+                Student student = new Student(id, name, surname, email, password, role, attendance, module);
                 return student;
 
-            case 2:
+            case "MANAGER":
                 int managerIncome = 0;
                 //TODO get income
-                User manager = new Manager(id, email, password, name, surname, roleId, managerIncome);
+                User manager = new Manager(id, email, password, name, surname, role, attendance, managerIncome);
                 return manager;
 
-            case 3:
+            case "MENTOR":
                 int mentorIncome = 0;
                 //TODO get income
-                User mentor = new Mentor(id, email, password, name, surname, roleId, mentorIncome);
+                User mentor = new Mentor(id, email, password, name, surname, role, attendance, mentorIncome);
                 return mentor;
 
-            case 4:
+            case "EMPLOYEE":
                 int employeeIncome = 0;
                 //TODO get income
-                User employee = new Mentor(id, email, password, name, surname, roleId, employeeIncome);
+                User employee = new Mentor(id, email, password, name, surname, role, attendance, employeeIncome);
                 return employee;
 
             default:
