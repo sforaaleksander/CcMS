50baf9c2024bf600b939fe6803ec06d17e1bf444
Alek <sforaaleksander@gmail.com>
2020-05-21 14:49:00 +0200

    start implementing checkAllStudentsAttendance() in MentorActions

--- src/main/java/org/codecool/ccms/session/MentorActions.java
+++ src/main/java/org/codecool/ccms/session/MentorActions.java
@@ -1,7 +1,12 @@
 package org.codecool.ccms.session;
 
 import org.codecool.ccms.controllers.MenuOption;
+import org.codecool.ccms.dao.UserDao;
+import org.codecool.ccms.modules.Displayable;
+import org.codecool.ccms.modules.Student;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MentorActions extends Actions {
@@ -22,11 +27,21 @@ public class MentorActions extends Actions {
     }
 
     public void gradeAssignment() {
-
     }
 
     public void checkAllStudentsAttendance() {
+        UserDao userDao = this.getSession().getUserDao();
+        String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        userDao.addWorkDay(date);
+        int workDayId = userDao.getWorkDayIdByDate(date).getDayId();
+        List<Displayable> students = userDao.getUserBy("roleId", "4");
+        for (Displayable displayStudent : students) {
+            Student student = (Student) displayStudent;
+            this.getSession().getView().displayMessage(student.getFirstName() + " " + student.getSurname());
+            String isPresent = this.getSession().getInputProvider().gatherYesNoInput("Is the student present?");
+            userDao.addAttendance(student.getId());
 
+        }
     }
 
     public void updateStudentAttendance(){
