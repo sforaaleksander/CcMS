a25e9b03cb8ed2e98f9bb97778d7a81b551af29a
Tomek Krogulec <tomek.krogulec@o2.pl>
2020-05-21 14:26:54 +0200

    implement EmpAndManActions class

---
+++ src/main/java/org/codecool/ccms/session/EmpAndManActions.java
@@ -0,0 +1,21 @@
+package org.codecool.ccms.session;
+
+import org.codecool.ccms.controllers.MenuOption;
+import java.util.List;
+
+public class EmpAndManActions extends Actions {
+
+
+    public EmpAndManActions(Session session) {
+        super(session);
+    }
+
+    @Override
+    public List<MenuOption> returnActions() {
+        return null;
+    }
+
+    public void viewAllStudentsContacts(){
+
+    }
+}
