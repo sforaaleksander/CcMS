fbc85440ce1de23a2d9fe6fe5b43d0eeb76be07a
Alek <sforaaleksander@gmail.com>
2020-05-21 22:22:50 +0200

    remove Id from WorkDay.java

--- src/main/java/org/codecool/ccms/modules/WorkDay.java
+++ src/main/java/org/codecool/ccms/modules/WorkDay.java
@@ -4,18 +4,12 @@ import java.time.LocalDate;
 
 public class WorkDay {
     private LocalDate date;
-    private int dayId;
 
-    public WorkDay(LocalDate date, int dayId) {
+    public WorkDay(LocalDate date) {
         this.date = date;
-        this.dayId = dayId;
     }
 
     public LocalDate getDate() {
         return date;
-    }
-
-    public int getDayId() {
-        return dayId;
     }
 }
