42e7b217693833c296552aa537b7681d69d7e05c
Tomek Krogulec <tomek.krogulec@o2.pl>
2020-05-21 18:10:52 +0200

    added updateMentorDetails method

--- src/main/java/org/codecool/ccms/session/EmpAndManActions.java
+++ src/main/java/org/codecool/ccms/session/EmpAndManActions.java
@@ -1,6 +1,8 @@
 package org.codecool.ccms.session;
 
 import org.codecool.ccms.controllers.MenuOption;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmpAndManActions extends Actions {
@@ -12,10 +14,51 @@ public class EmpAndManActions extends Actions {
 
     @Override
     public List<MenuOption> returnActions() {
-        return null;
+        List<MenuOption> options = new ArrayList<>();
+        options.add(new MenuOption("Update mentor details.", this::updateMentorDetails));
+        options.add(new MenuOption( "View all students.", this::viewAllStudents));
+        options.add(new MenuOption( "Add student", this::addStudent));
+        options.add(new MenuOption( "Remove student", this::removeStudent));
+        return options;
     }
 
-    public void viewAllStudentsContacts(){
+    public void updateMentorDetails(){
+        String id = this.getSession().getInputProvider().gatherInput("Enter mentor's ID you would like to edit: ");
+        System.out.println("What you want to update:\n1. Name\n2. Surname\n3. Email\n4. All");
+        String choice = this.getSession().getInputProvider().gatherInput("Choice: ");
+        switch (choice) {
+            case "1":
+                String name = this.getSession().getInputProvider().gatherInput("Provide new name.");
+                this.getSession().getUserDao().updateUser(id, "first_name", name);
+                break;
+            case "2":
+                String surname = this.getSession().getInputProvider().gatherInput("Provide new surname.");
+                this.getSession().getUserDao().updateUser(id, "surname", surname);
+                break;
+            case "3":
+                String email = this.getSession().getInputProvider().gatherInput("Provide new email.");
+                this.getSession().getUserDao().updateUser(id, "email", email);
+                break;
+            case "4":
+                String name1 = this.getSession().getInputProvider().gatherInput("Provide new name.");
+                this.getSession().getUserDao().updateUser(id, "first_name", name1);
+                String surname1 = this.getSession().getInputProvider().gatherInput("Provide new surname.");
+                this.getSession().getUserDao().updateUser(id, "surname", surname1);
+                String email1 = this.getSession().getInputProvider().gatherInput("Provide new email.");
+                this.getSession().getUserDao().updateUser(id, "email", email1);
+                break;
+        }
+    }
+
+    public void viewAllStudents(){
+
+    }
+
+    public void addStudent(){
+
+    }
+
+    public void removeStudent(){
 
     }
 }
