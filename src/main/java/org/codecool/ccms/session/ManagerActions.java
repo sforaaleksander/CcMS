0dc277c42b2dfe2337eadf40c3400aedfcc593ba
Tomek Krogulec <tomek.krogulec@o2.pl>
2020-05-21 13:44:55 +0200

    implement ManagerActions class

---
+++ src/main/java/org/codecool/ccms/session/ManagerActions.java
@@ -0,0 +1,34 @@
+package org.codecool.ccms.session;
+
+import org.codecool.ccms.controllers.MenuOption;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManagerActions extends Actions {
+
+    public ManagerActions(Session session) {
+        super(session);
+    }
+
+    @Override
+    public List<MenuOption> returnActions() {
+        List<MenuOption> options = new ArrayList<>();
+        options.add(new MenuOption(0,"View all mentors.", this::viewAllMentors));
+        options.add(new MenuOption(0, "Remove mentor", this::removeMentor));
+        options.add(new MenuOption(0, "Add mentor.", this::addMentor));
+        return options;
+    }
+
+    public void viewAllMentors(){
+
+    }
+
+    public void removeMentor(){
+
+    }
+
+    public void addMentor(){
+
+    }
+}
