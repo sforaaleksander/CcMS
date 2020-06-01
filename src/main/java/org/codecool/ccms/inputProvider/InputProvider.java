3f06dde5a079151b3c448e0c6058b99c25f32ec6
pirat77 <mcyanek@gmail.com>
2020-06-01 14:55:59 +0200

    Refactors inputprovider class, removing code repetitions, allowing validation and script execution in one place

--- src/main/java/org/codecool/ccms/inputProvider/InputProvider.java
+++ src/main/java/org/codecool/ccms/inputProvider/InputProvider.java
@@ -4,125 +4,106 @@ package org.codecool.ccms.inputProvider;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-import java.util.Scanner;
-import java.util.Stack;
-
+import java.util.*;
 
 
 public class InputProvider {
     public Scanner scan;
-    private final Stack<String> commands;
+    private final Stack<String> commandsStack;
 
     public InputProvider(String[] args) {
         scan = new Scanner(System.in);
         scan.useDelimiter(System.lineSeparator());
-        commands = new Stack<>();
+        commandsStack = new Stack<>();
         initializeStack(args);
     }
 
     private void initializeStack(String[] args){
-        Stack<String> midStack = new Stack<>();
+        Iterator<String> commands = getCommandsFromFile(args).iterator();
+        while (commands.hasNext()){
+            commandsStack.push(commands.next());
+        }
+    }
+
+    private List<String> getCommandsFromFile(String[] args){
+        List<String> commands = new LinkedList<>();
         try {
             File file = new File(args[0]);
             Scanner scanFile = new Scanner(file);
-
             while (scanFile.hasNextLine()) {
-                midStack.push(scanFile.nextLine());
+                commands.add(0, scanFile.nextLine());
             }
             scanFile.close();
         } catch (ArrayIndexOutOfBoundsException | FileNotFoundException e) {
             e.getMessage();
         }
-        while (!midStack.isEmpty()){
-            commands.push(midStack.pop());
+        return commands;
+    }
+
+    private String getInput(){
+        if (commandsStack.isEmpty()) {
+            return validateAgainstSQLinjection(scan.next());
+        }
+        else {
+            return validateAgainstSQLinjection(String.valueOf(commandsStack.pop()));
         }
     }
 
-    private String tryToGrabStringFromStack(){
-        if (commands.isEmpty()){
-            return scan.next();
+    private String validateAgainstSQLinjection(String input){
+        // TODO: validate
+        return input;
+    }
+
+    private boolean isInputInt(String userInput) {
+        if (userInput.matches("^[0-9]*$")) { return true; }
+        else { return false; }
+    }
+
+    private boolean isNumberInRange(String userInput, int rangeMin, int rangeMax) {
+        if (userInput.matches("^[0-9]*$")) {
+            int userInt = Integer.parseInt(userInput);
+            return userInt >= rangeMin && userInt <= rangeMax;
         }
-        return String.valueOf(commands.pop());
+        return false;
     }
 
-    public String gatherInput(String title) {
-        System.out.println(title);
-        boolean validInput = true;
-        String userInput;
-        do {
-            if (!validInput) {
-                System.out.println("Your input must contain at least one character. Enter again: ");
-            }
-            validInput = false;
-
-            userInput = tryToGrabStringFromStack();
-
-            if (!userInput.equals("")) {
-                validInput = true;
-            }
-        } while (!validInput);
+    public String gatherInput(String message) {
+        System.out.println(message);
+        String userInput =getInput();
+        while (userInput == ""){
+            System.out.println("Your input must contain at least one character. Enter again: ");
+            userInput = getInput();
+        }
         return userInput;
     }
 
-    public String gatherYesNoInput(String title) {
-        System.out.println(title + " [Y/N]");
-        boolean validInput = true;
-        String userInput;
-        do {
-            if (!validInput) {
-                System.out.println("Invalid input. Enter again: ");
-            }
-            validInput = false;
-            userInput = tryToGrabStringFromStack().toUpperCase();
-            if (userInput.equals("Y") || userInput.equals("N")) {
-                validInput = true;
-            }
-        } while (!validInput);
+    public String gatherYesNoInput(String message) {
+        System.out.println(message + " [Y/N]");
+        String userInput = getInput();
+        while (!(userInput.equals("Y") || userInput.equals("N") ))
+            System.out.println("Invalid input. Choose one: [Y/N] ");
+            userInput = gatherInput("").toUpperCase();
         return userInput;
     }
-
 
     public int gatherIntInput(String message, int rangeMin, int rangeMax) {
         System.out.println(message);
-        String userInput = "";
-        boolean validInput = false;
-        while (!validInput) {
-            userInput = tryToGrabStringFromStack();
-            validInput = isNumberInRange(userInput, rangeMin, rangeMax-1);
+        String userInput = getInput();
+        while (!isNumberInRange(userInput, rangeMin, rangeMax-1)) {
+            System.out.println("Invalid input, please try again: ");
+            userInput = gatherInput("");
         }
         return Integer.parseInt(userInput);
     }
 
     public int gatherIntInput(String message) {
         System.out.println(message);
-        String userInput = "";
-        boolean validInput = false;
-        while (!validInput) {
-            userInput = tryToGrabStringFromStack();
-            validInput = isInputInt(userInput);
+        String userInput = getInput();
+        while (!isInputInt(userInput)) {
+            System.out.println("Provide numeric input, please.");
+            userInput = gatherInput("");
         }
         return Integer.parseInt(userInput);
     }
 
-    private boolean isInputInt(String userInput) {
-        if (!userInput.equals("")) {
-            if (userInput.matches("^[0-9]*$")) {
-                return true;
-            }
-        }
-        System.out.println("Invalid input, please try again: ");
-        return false;
-    }
-
-    private boolean isNumberInRange(String userInput, int rangeMin, int rangeMax) {
-        int userInt;
-        if (!userInput.equals("")) {
-            if (userInput.matches("^[0-9]*$")) {
-                userInt = Integer.parseInt(userInput);
-                return userInt >= rangeMin && userInt <= rangeMax;
-            }
-        }
-        System.out.println("Invalid input, please try again: ");
-        return false;
-    }
 }
