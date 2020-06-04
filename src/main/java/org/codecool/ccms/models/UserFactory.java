638838a08fc3dfb75fe0ca9956cadc47d3c107e1
Tomek Krogulec <tomek.krogulec@o2.pl>
2020-06-04 18:07:30 +0200

    added salt parameter

--- src/main/java/org/codecool/ccms/models/UserFactory.java
+++ src/main/java/org/codecool/ccms/models/UserFactory.java
@@ -8,30 +8,30 @@ public class UserFactory {
     }
 
     public User makeUser(int id, String name, String surname, String email,
-                         String password, Role role, Attendance attendance) {
+                         String password, Role role, Attendance attendance, byte[] salt) {
 
         switch (role.toString()){
             case "STUDENT":
                 Module module = new UserCrossAssignmentSQLDao().getStudentModuleBasedOnPassedAssignments(id);
-                Student student = new Student(id, name, surname, email, password, role, attendance, module);
+                Student student = new Student(id, name, surname, email, password, role, attendance, salt, module);
                 return student;
 
             case "MANAGER":
                 int managerIncome = 0;
                 //TODO get income
-                User manager = new Manager(id, name, surname, email, password, role, attendance, managerIncome);
+                User manager = new Manager(id, name, surname, email, password, role, attendance, salt, managerIncome);
                 return manager;
 
             case "MENTOR":
                 int mentorIncome = 0;
                 //TODO get income
-                User mentor = new Mentor(id, name, surname, email, password, role, attendance, mentorIncome);
+                User mentor = new Mentor(id, name, surname, email, password, role, attendance, salt, mentorIncome);
                 return mentor;
 
             case "EMPLOYEE":
                 int employeeIncome = 0;
                 //TODO get income
-                User employee = new Mentor(id, name, surname, email, password, role, attendance, employeeIncome);
+                User employee = new Mentor(id, name, surname, email, password, role, attendance, salt, employeeIncome);
                 return employee;
 
             default:
